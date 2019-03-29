0aaf8e821898b3e061ab6acd83580e5aa3568827
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-28 20:53:01 -0700

    restructered file orginization

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-       (accessToken, refreshToken, profile, done) => {
-            console.log(` Access Token: ${accessToken}`);
-            console.log(`Refresh Token: ${refreshToken}`);
-            console.log(`Profile: ${JSON.stringify(profile)}`);
-        }
-    )
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-    '/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
 
