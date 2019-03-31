00bbdfe4655869c4ef8af55cdab036b3a193fc0b
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-30 20:08:52 -0700

    installed mongoose and starting to setup db for users

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require("mongoose");
 
+require('./services/passport');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+// If deployed, use the deployed database. Otherwise use the local feedBackApp database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/feedBackApp";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
