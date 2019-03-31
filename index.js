29fd0dbec2c5177e4581791f7a485a2141226eee
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-30 23:41:02 -0700

    installed cookie-session and finished oauth and mongoDB setup

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const mongoose = require("mongoose");
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
-
-// If deployed, use the deployed database. Otherwise use the local feedBackApp database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/feedBackApp";
-
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
