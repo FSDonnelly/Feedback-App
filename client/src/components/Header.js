22b1d674707b212d4b4b03c37ed8c02a914d84ae
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-02 03:21:51 -0700

    setup authReducers values and accessing state in the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still waiting'
+            case false:
+                return 'Logged Out'
+            default:
+                return 'Logged In'
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo">Feedback App</a>
                     <ul id="nav-mobile" className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
+                       { this.renderContent() }
                     </ul>
                 </div>
             </nav>
@@ -17,4 +27,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
