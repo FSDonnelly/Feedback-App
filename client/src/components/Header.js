c2ddd9e5933d6b98a2853c766bf07b27620f79c0
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-02 02:59:53 -0700

    refactored fetch_user action in src/actions/index.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">Feedback App</a>
+                    <ul id="nav-mobile" className="right">
+                        <li>
+                            <a>Login with Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;
