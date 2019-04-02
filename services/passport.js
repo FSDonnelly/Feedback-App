3965b446528b75dcf1e802ddcabde0cc85dc6f77
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-01 23:17:35 -0700

    converted passport.js to async await functions instead of Promises

--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,16 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleID: profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        // we already have record with given ID
-                        done(null, existingUser);
-                    } else {
-                        new User({ googleID: profile.id })
-                            .save()
-                            .then(user => done(null, user));
-                    }
-                });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleID: profile.id });
+
+            if (existingUser) {
+                // we already have record with given ID
+                done(null, existingUser);
+            } else {
+                const user = await new User({ googleID: profile.id }).save();
+                done(null, user);
+            }
         }
     )
 );
