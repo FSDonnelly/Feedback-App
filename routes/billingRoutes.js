33a83228cf2213283b9e22d68e41c0598a260727
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-05 22:46:10 -0700

    setup stripe payment on the backend

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,12 +3,20 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
+        if (!req.user) {
+            return res.status(401).send({ error: 'You must sign in!' });
+        };
+
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 Credits',
             source: req.body.id
         });
-        console.log(charge)
+
+        req.user.credits += 5;
+        const user = await req.user.save();
+
+        res.send(user);
     });
 };
