2dd44ebc69fc2bf94e3154b264b76587be69497a
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-06 23:34:56 -0700

    added logic in index.js to handle express routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/user');
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -25,6 +25,15 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
