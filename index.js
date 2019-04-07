41e0070b3cb83633fa011c9ba701c119911cc037
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-07 00:05:52 -0700

    added heroku-postbuild script to package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/User');
+require('./models/user');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
