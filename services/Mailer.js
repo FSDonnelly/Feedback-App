7d8b23aac68dfa16fdc12e350a2030c26fc0ea64
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-09 01:34:23 -0700

    finished mailer setup

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@feedback-app.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -13,6 +14,7 @@ class Mailer extends helper.Mail {
 
         this.addContent(this.body);
         this.addClickTracking();
+        this.addRecipients();
     }
 
     formatAddresses(recipients) {
@@ -23,10 +25,30 @@ class Mailer extends helper.Mail {
 
     addClickTracking() {
         const trackingSettings = new helper.TrackingSettings();
-        const clickTracking = new helper.ClickTracking(true, true); 
+        const clickTracking = new helper.ClickTracking(true, true);
 
         trackingSettings.setClickTracking(clickTracking);
         this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = this.sgApi.API(request);
+        return response;
     }
 }
 
