d447dfa246f3afaef6840c84830b827233cefb32
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-11 22:14:29 -0700

    started SurveyNew component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard
             <div className="fixed-action-btn">
-                <a className="btn-floating btn-large red">
-                    <i className="large material-icons">+</i>
-                </a>
+                <Link className="btn-floating btn-large red" to="/surveys/new">
+                    <i className="large material-icons">create</i>
+                </Link>
             </div>
         </div>
     );
