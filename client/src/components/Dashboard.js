1e5637101ffb17e9371e4d8763ba9f0166e6a16a
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-11 21:19:19 -0700

    started Dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard
+            <div className="fixed-action-btn">
+                <a className="btn-floating btn-large red">
+                    <i className="large material-icons">+</i>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
