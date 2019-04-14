85efd7b62014654250ae7bac6a459649fdf268a0
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-14 01:26:53 -0700

    refactored surveyForm to surveyFormReview

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyReview = ({ onCancel, formValues }) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey }) => {
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
             <div key={name}>
@@ -19,10 +20,11 @@ const SurveyReview = ({ onCancel, formValues }) => {
         <div>
             <h5>Please confirm your survey enties</h5>
             {reviewFields}
-            <button className="yellow darken-3 btn-flat" onClick={onCancel} >
+            <button className="red darken-3 btn-flat white-text" onClick={onCancel} >
                 Back
             <i className="material-icons left">backspace</i>
             </button>
+            <button onClick={() => submitSurvey(formValues)} className="teal btn-flat right white-text">Send Survey <i className="material-icons right">email</i></button>
         </div>
     );
 };
@@ -31,4 +33,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 };
 
-export default connect(mapStateToProps)(SurveyReview);
+export default connect(mapStateToProps, actions)(SurveyReview);
