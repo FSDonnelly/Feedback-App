e893a55ef87855848e17b1a865eeb7696166c3cc
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-13 23:51:59 -0700

    started surveyFormReview

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -30,7 +30,7 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                <form onSubmit={this.props.handleSubmit(() => this.props.onSurveySubmit())}>
                     {this.renderFields()}
                     <button type="submit" className="teal btn-flat right white-text">Next <i className="material-icons right">navigate_next</i></button>
                     <Link to="/surveys" className="red btn-flat left white-text">Cancel<i className="material-icons left">cancel</i></Link>
---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SurveyReview = () => {
+    return (
+        <div>
+            <h5>Please confirm your survey enties</h5>
+        </div>
+    );
+};
+
+export default SurveyReview;
