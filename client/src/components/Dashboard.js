27278f53d3932b2a90162e09f08529dbcf365151
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-17 23:48:25 -0700

    created SurveyList component to render in the Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
+            <SurveyList />
             <div className="fixed-action-btn container">
                 <Link className="btn-floating btn-large red" to="/surveys/new">
                     <i className="large material-icons">create</i>
